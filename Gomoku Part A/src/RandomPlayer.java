5a83e656a045fffd917b82ddc53c994ae6f5bb52
Oblynx <Oblynx@users.noreply.github.com>
2014-11-17 14:08:08 +0200

    Added wistful comments

--- Gomoku Part A/src/RandomPlayer.java
+++ Gomoku Part A/src/RandomPlayer.java
@@ -1,25 +1,44 @@
-
+//The following class embodies the spirit of our latest software
 public class RandomPlayer implements AbstractPlayer
 {
-    private int id, score;
+    private int id, score;					//These are the properties of our class
     private String name;
 
+    /**This is a simple constructor that takes only the name of the new player
+     * @param pid
+     */
     public RandomPlayer(Integer pid) {
 	id = pid.intValue();
     }
-
+    
+    /**Whereas this particular one is a complex player-generating constructor machine, that defines not only the players' identity
+     * but also many other fabulous properties
+     * @param pid
+     * @param name0
+     * @param score0
+     */
     public RandomPlayer(Integer pid,String name0,int score0) {
 	id = pid.intValue();
 	name = name0;
 	score = score0;
     }
 
+    /**What follows is a magnificent streak of get/set methods. They are all here; so much so that defining private properties
+     * is actually very useless
+     * @return
+     */
     public int getId() {return id;}
     public void setId(int a) {id=a;}
     public String getName() {return name;}
     public void setName(String a) {name=a;}
     public int getScore() {return score;}
     public void setScore(int a) {score=a;}
+    
+    /**And now marvel at the beating heart of our RandomPlayer:
+     * you have the unique chance of admiring a method that returns 2 random numbers
+     * but with great significance. They personify our next move on the board
+     * @param board
+     */
     public int[] getNextMove( Board board ) {
 	int[] xy = new int[2];
 
@@ -31,3 +50,6 @@ public class RandomPlayer implements AbstractPlayer
 	return xy;
     }
 }
+/* Thank you for your attention
+ * Sincerely, a domes_dedomenon Team
+ */
--- Gomoku Part A/src/Tile.java
+++ Gomoku Part A/src/Tile.java
@@ -1,9 +1,18 @@
  
 public class Tile implements Cloneable
 {
+	//Tile::properties
 	private	int id, x, y, color, playerId;
 	private boolean mark;
 	
+	/**The Tile class constructor
+	 * @param id0
+	 * @param x0
+	 * @param y0
+	 * @param col0
+	 * @param mark0
+	 * @param plId0
+	 */
 	public Tile(int id0, int x0, int y0, int col0, boolean mark0, int plId0){
 		id=id0;
 		x=x0; y=y0;
@@ -12,6 +21,9 @@ public class Tile implements Cloneable
 		playerId=plId0;
 	}
 	
+	/** The Tile class reknowned get/set methods
+	 * @return
+	 */
 	public int getId() {return id;}
 	public void setId(int a) {id=a;}
 	public int getX() {return x;}
@@ -26,3 +38,4 @@ public class Tile implements Cloneable
 	public void setMark(boolean a) {mark=a;}
 	
 }
+//That's all, thank you.
