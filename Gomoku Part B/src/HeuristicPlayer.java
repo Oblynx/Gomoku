13a48979cefde406e8c0384f8b827c7dc5fd9c1a
Oblynx <Oblynx@users.noreply.github.com>
2014-12-04 12:46:08 +0200

    Completed getNextMove() method. Possibly revise

--- Gomoku Part B/src/HeuristicPlayer.java
+++ Gomoku Part B/src/HeuristicPlayer.java
@@ -1,5 +1,4 @@
-
-
+import java.util.ArrayList;
 
 public class HeuristicPlayer implements AbstractPlayer
 {
@@ -60,12 +59,29 @@ public class HeuristicPlayer implements AbstractPlayer
 
   public int[] getNextMove (Board board)
   {
-     // TODO
-  
+	 // TODO
+	 ArrayList<int[]> evaluatedPos= new ArrayList<int[]>();
+	 int[] singleEvalPos= new int[3], bestMove= new int[3];
+	 bestMove[2]= Integer.MIN_VALUE;
+	 //How to get nRows, nColumns?
+	 for(int x=0; x<GomokuUtilities.NUMBER_OF_COLUMNS; x++)
+		 for(int y=0; y<GomokuUtilities.NUMBER_OF_ROWS; y++){
+			 singleEvalPos[0]= x; singleEvalPos[1]= y;			//HATE Java for not allowing me to do this in the <for>!!!
+			 singleEvalPos[2]= evaluate(x,y, board);
+			 if (bestMove[2] < singleEvalPos[2])
+				 bestMove= (int[]) singleEvalPos.clone();		//My exasperated intent is to DEEP COPY the old object into the new.
+			 												//Tell me, dear Java, is this too much to ask of you??? :(
+			 evaluatedPos.add(singleEvalPos);
+		 }
+	 //Why did you tell us to use ArrayList???
+	 return bestMove;
   }
   
   int evaluate (int x, int y, Board board){
   	  // TODO
+	  
+	  
+	  return 0;
   }
 
 }
