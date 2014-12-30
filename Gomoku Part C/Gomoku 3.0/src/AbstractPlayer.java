39577025744fff9ff354d57403f94edbd02ca878
Oblynx <Oblynx@users.noreply.github.com>
2014-12-31 00:06:07 +0200

    Final parts A,B and part C base

---
+++ Gomoku Part C/Gomoku 3.0/src/AbstractPlayer.java
@@ -0,0 +1,20 @@
+
+
+
+public interface AbstractPlayer
+{
+  public void setId (int id);
+
+  public int getId ();
+
+  public void setName (String name);
+
+  public String getName ();
+
+  public void setScore (int score);
+
+  public int getScore ();
+
+  public int[] getNextMove (Board board);
+
+}
