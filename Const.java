ae89ac4f635a81833f35ba7b2ffbbd5013fa9c56
kipoujr <kipoujr@hotmail.com>
2015-01-07 22:46:42 +0200

    Minmax exei douleia

---
+++ Const.java
@@ -0,0 +1,7 @@
+class Const {
+	static int roi= 5;
+	static int columns = GomokuUtilities.NUMBER_OF_COLUMNS;
+	static int rows = GomokuUtilities.NUMBER_OF_ROWS;
+	static int[] dx = {0,1,1,1,0,-1,-1,-1};
+	static int[] dy = {1,1,0,-1,-1,-1,0,1};
+}
